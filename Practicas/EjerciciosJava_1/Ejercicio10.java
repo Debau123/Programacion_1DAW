141d9853e15b7be821d675ef5535c25287194ddb
iñaki <debau@hotmail.es>
2023-10-18 18:24:46 +0200

    añadidos cambios

--- Practicas/EjerciciosJava_1/Ejercicio10.java
+++ Practicas/EjerciciosJava_1/Ejercicio10.java
@@ -13,7 +13,20 @@ public class Ejercicio10 {
         int suma = numero1 + numero2;
         int resta = numero1 - numero2;
         int multi = numero1 * numero2;
-        int division = numero1 / numero2;
-        System.out.println("La suma es "+ suma + " La resta es "+ resta +" La multiplicacion es " + multi +" La division es "+ division );
+        if (numero1==0){
+            System.out.println("No se puede dividir por 0");}
+        else if (numero2==0) {
+            System.out.println("No se puede dividir por 0");}
+        else {
+            int division = numero1/numero2;
+                    System.out.println("La division es"+ division);
+        }
+        System.out.println("La suma es " + suma);
+        System.out.println("La resta es " + resta);
+        System.out.println("La multiplicación es " + multi);
+
     }
+
 }
+
+
