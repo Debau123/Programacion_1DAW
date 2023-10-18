984d9f66f843ee10c1f4db355d68205b5135f4bc
iñaki <debau@hotmail.es>
2023-10-18 23:58:04 +0200

    ÂMuchos ejercicios

---
+++ Practicas/EjerciciosJava_1/Ejercicio13.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Ejercicio13 {
+    public static void main(String[] args) {
+        Scanner Entrada = new Scanner(System.in);
+        System.out.println("Dame un numero");
+        int Primer_numero = Entrada.nextInt();
+
+        if (Primer_numero > 0) {
+            System.out.println("El numero dado es positivo ");
+        } else {
+            System.out.println("El numero dado es negativo  ");
+        }
+    }
+}
