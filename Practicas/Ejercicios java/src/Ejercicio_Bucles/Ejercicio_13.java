784ecb585f09950b0131feba06dd527016889977
iñaki <debau@hotmail.es>
2023-10-28 16:24:44 +0200

    aFeat: Terminado ejercicio bucles

--- Practicas/Ejercicios java/src/Ejercicio_Bucles/Ejercicio_13.java
+++ Practicas/Ejercicios java/src/Ejercicio_Bucles/Ejercicio_13.java
@@ -1,7 +1,42 @@
 package Ejercicio_Bucles;
 
+import java.util.Scanner;
+
 public class Ejercicio_13 {
     public static void main(String[] args) {
+            Scanner entrada= new Scanner(System.in);
+            int minimo=1;
+            int maximo=100;
+            int intentos=1;
 
+        System.out.println("Piensa en un número del 1 al 100 y yo intentaré adivinarlo.");
+        System.out.println("Cuando estés listo, ingresa 'igual' para comenzar.");
+
+        String respuesta= entrada.next();
+        if(!respuesta.equalsIgnoreCase("igual")){
+            System.out.println("Debes introducir iguals para comenzar");
+        }
+
+        while (true){
+            int intento = (minimo + maximo)/2;
+            System.out.println("¿Es " + intento + " tu número?");
+            System.out.println("Ingresa 'mayor' si es mayor, 'menor' si es menor, o 'igual' si he adivinado:");
+
+            respuesta= entrada.next();
+
+            if(!respuesta.equalsIgnoreCase("igual")){
+                System.out.println("Lo hemos adivinado este es tu numero "+ intento+ " en "+intentos+" intentos");
+                break;
+            } else if (respuesta.equalsIgnoreCase("mayor")) {
+                minimo = intento + 1;
+            } else if (respuesta.equalsIgnoreCase("menor")) {
+                maximo = intento - 1;
+            } else {
+                System.out.println("Por favor, ingresa 'mayor', 'menor' o 'igual'.");
+            }
+            intentos++;
+            }
+
+        }
     }
-}
+
--- Practicas/Ejercicios java/src/Ejercicio_Bucles/Ejercicio_14.java
+++ Practicas/Ejercicios java/src/Ejercicio_Bucles/Ejercicio_14.java
@@ -1,4 +1,51 @@
 package Ejercicio_Bucles;
 
+import java.util.Scanner;
+
 public class Ejercicio_14 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Dime el dinero que te quieres gastar te dire los billetes que necesitas:");
+        System.out.println("Tiene que ser multiplo de 5 ");
+        int billetes = entrada.nextInt();
+        int billete= billetes;
+        int billete_500 = 0;
+        int billete_200 = 0;
+        int billete_100 = 0;
+        int billete_50 = 0;
+        int billete_20 = 0;
+        int billete_10 = 0;
+        int billete_5 = 0;
+        if (billetes % 5 > 0) {
+            System.out.println("Introduce un numero multiple de 5");
+        }while (true){
+            if(billete >=500){
+                billete -= 500;
+                billete_500++;
+            } else if (billete >=200){
+                billete -= 200;
+                billete_200++;
+            } else if(billete >=100){
+                billete -= 100;
+                billete_100++;
+            } else if (billete >=50){
+                billete -= 50;
+                billete_50++;
+            } else if (billete >=20){
+                billete -= 20;
+                billete_20++;
+            } else if(billete >=10){
+                billete -= 10;
+                billete_10++;
+            } else if (billete >=5) {
+                billete -= 5;
+                billete_5++;
+            }else
+                break;
+
+        }
+        System.out.println("Para pagar "+billetes+" necesitaras "+billete_500+" billetes de 500,  "+billete_200+" billetes de 200,  "
+                +billete_100+" billetes de 100,  "+billete_50+" billetes de 50,  "+billete_20+" billetes de 20,  "+billete_10+" billetes de 10,  " +
+               +billete_5+ " billetes de 5.");
+    }
 }
